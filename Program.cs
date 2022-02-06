5ccc83d81d192905ad4d5cf6fd30849e400b09fa
rahul <rahulravi2502@gmail.com>
2022-02-06 12:09:44 +0530

    Adding The reversed Linked LISt

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace LinkefListProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello welcome to Linked List Problem");
+            Program program = new Program();
+            program.LinkedListCall();
+            Console.ReadLine();
+        }
+
+        public void LinkedListCall()
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddLastNode(56);
+            customLinkedList.AddLastNode(30);
+            customLinkedList.AddLastNode(76);
+            Console.WriteLine("nodes inserted");
+            customLinkedList.DisplayLinkedList();
+
+            customLinkedList.AddFirstNode(70);
+            customLinkedList.AddFirstNode(30);
+            customLinkedList.AddFirstNode(56);
+            customLinkedList.DisplayLinkedList();
         }
     }
 }
+
