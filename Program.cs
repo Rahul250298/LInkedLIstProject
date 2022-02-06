0b979c664edf2b6c58a79115fdd7c0f35238758e
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-02-06 12:00:11 +0530

    Adding program

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace LinkefListProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello welcome to Linked List Problem");
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddNode(56);
+            customLinkedList.AddNode(30);
+            customLinkedList.AddNode(76);
+            customLinkedList.AddNode(80);
+            Console.ReadLine();
         }
     }
 }
